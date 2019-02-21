d92de1b23a362647347be7a2ebdbd40ab911a9ac
SubPointSupport <support@subpointsolutions.com>
2019-02-21 21:46:53 +1100

    + minor fixes

--- uplift-vagrant/crm-dev/scripts-crm/crm-dynamics-download.ps1
+++ uplift-vagrant/crm-dev/scripts-crm/crm-dynamics-download.ps1
@@ -8,6 +8,7 @@ Write-UpliftEnv
 
 $resourceName = Get-UpliftEnvVariable "UPLF_CRM_RESOURCE_NAME" "" "ms-dynamics-crm90-server-en"
 $serverUrl    = Get-UpliftEnvVariable "UPLF_HTTP_ADDR"
+
 $uplifLocalRepository =  Get-UpliftEnvVariable "UPLF_LOCAL_REPOSITORY_PATH" "" "c:/_uplift_resources"
 
 # always turn into http, it might be 10.0.2.2 address only
@@ -21,8 +22,10 @@ pwsh -c Invoke-Uplift resource download-local $resourceName -server $serverUrl -
 
 Write-UpliftMessage "Unpacking resource: $resourceName"
 
-$filePath = "C:\_uplift_resources\$resourceName\latest\CRM9.0-Server-ENU-amd64.exe"
-$directoryPath =  "C:\_uplift_resources\$resourceName\latest\unpacked"
+$resourceLatestFolder = "$uplifLocalRepository\$resourceName\latest"
+$filePath = Find-UpliftFileInPath $resourceLatestFolder
+
+$directoryPath =  "$uplifLocalRepository\$resourceName\latest\unpacked"
 
 Start-Process -FilePath $filePath -ArgumentList "/extract:$directoryPath /passive /quiet" -Wait -NoNewWindow
 
